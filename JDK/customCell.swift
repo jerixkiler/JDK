f330e676bef2c33e4e9dde9f2e99feec34faf456
jerixkiler <jerixkiller3@gmail.com>
2017-05-31 15:46:05 +0800

    added new design

---
+++ JDK/customCell.swift
@@ -0,0 +1,16 @@
+//
+//  customCell.swift
+//  JDK
+//
+//  Created by Nexusbond on 31/05/2017.
+//  Copyright © 2017 Nexusbond. All rights reserved.
+//
+
+import UIKit
+
+class customCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
+}
---
+++ JDK/topicController.swift
@@ -0,0 +1,58 @@
+//
+//  topicController.swift
+//  JDK
+//
+//  Created by Nexusbond on 31/05/2017.
+//  Copyright © 2017 Nexusbond. All rights reserved.
+//
+
+import UIKit
+
+class topicController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var images = ["one", "two", "three", "four", "five", "six", "seven"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCell", for: indexPath) as! customCell
+        
+        cell.myImage.image = UIImage(named: images[indexPath.row])
+        
+        return cell
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
