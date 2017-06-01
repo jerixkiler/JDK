7d29b8f264e5cf57f79f69f457b713c447bc62d8
jerixkiler <jerixkiller3@gmail.com>
2017-06-01 18:01:07 +0800

    yahay

---
+++ JDK/SeeMemberViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  SeeMemberViewCell.swift
+//  JDK
+//
+//  Created by Nexusbond on 01/06/2017.
+//  Copyright © 2017 Nexusbond. All rights reserved.
+//
+
+import UIKit
+
+class SeeMemberViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var btnStatus: UIButton!
+    @IBOutlet weak var displayNameText: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    override func layoutSubviews() {
+        profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width / 2
+    }
+    
+}
